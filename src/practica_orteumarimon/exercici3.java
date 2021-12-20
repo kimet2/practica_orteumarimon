62c218a752f5461a7a61b18ea4d2f9c2ce9e9655
kimet2 <joaquimorteu1@gmail.com>
2021-12-20 10:47:57 +0100

    master

---
+++ src/practica_orteumarimon/exercici3.java
@@ -0,0 +1,20 @@
+package practica_orteumarimon;
+
+public class exercici3 {
+
+		public static void main(String[] args) {
+			printOne();
+			printOne();
+			printTwo();
+		}
+
+		public static void printOne() {
+			System.out.println("Hello World");
+		}
+
+		public static void printTwo() {
+			printOne();
+			printOne();
+		}
+
+}
